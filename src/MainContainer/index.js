9a2d4127939988a033ecd01ff77f6336f660f785
Isaiah Harris <harrisfambam@gmail.com>
2019-01-15 21:54:10 -1000

    install react router dom

--- src/MainContainer/index.js
+++ src/MainContainer/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Home from '../Home';
 class MainContainer extends Component {
   render() {
     return (
       <div className="main-container">
         <Switch>
-          <Route exact path="/" component={home} />
+          <Route exact path="/" component={Home} />
         </Switch>
       </div>
     );
