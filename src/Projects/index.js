c12ae6f51dfa28b10379ee644db8aafcfeab9dfa
Isaiah Harris <harrisfambam@gmail.com>
2019-01-16 10:50:09 -1000

    setup front end page layout and css structure

--- src/Projects/index.js
+++ src/Projects/index.js
@@ -22,13 +22,29 @@ class Projects extends Component {
       <div className="projects-container">
         <div className="header">Project 1</div>
         <div className="body-container">
-          {this.props.projects[0] && (
-            <img
-              src={this.props.projects[0].image_url}
-              alt=""
-              className="project"
-            />
-          )}
+          <div className="image-container">
+            {this.props.projects[0] && (
+              <img
+                src={this.props.projects[0].image_url}
+                alt=""
+                className="project"
+              />
+            )}
+          </div>
+          <div className="spool-pieces-container">
+            {this.props.projects[0] &&
+              this.props.projects[0].spoolPieces.map((piece, key) => {
+                console.log(piece);
+                return (
+                  <div className="spool-piece" key={key}>
+                    <div className="spool-piece-title">{piece.title}</div>
+                    <div className="spool-piece-img">
+                      <img src={piece.image_url} alt="" />
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
         </div>
       </div>
     );
