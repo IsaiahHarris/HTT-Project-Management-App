6a318c369fcb6178672d3372b1b25974b5c00c3b
Isaiah Harris <harrisfambam@gmail.com>
2019-01-19 16:44:40 -1000

    set height for main-container

--- src/MainContainer/index.js
+++ src/MainContainer/index.js
@@ -10,7 +10,7 @@ class MainContainer extends Component {
       <div className="main-container">
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/project" component={Projects} />
           <Route exact path="/login" component={Login} />
         </Switch>
       </div>
