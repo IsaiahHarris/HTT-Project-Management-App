8b0547dca14461a030af4f635720b69e5f0368aa
Isaiah Harris <harrisfambam@gmail.com>
2019-01-19 16:43:56 -1000

    set up project route

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Project = require('../db/models/Project');
 const spoolPieces = require('../db/models/Spool_Piece');
 router.route('/').get((req, res) => {
-  return Project.fetchAll({ withRelated: ['spoolPieces'] })
+  return Project.fetchAll({ withRelated: ['spoolPieces', 'users'] })
     .then(projects => {
       return res.json(projects);
     })
