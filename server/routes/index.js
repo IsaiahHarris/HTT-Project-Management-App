167f1506462b887da3b944f76707a3e69e9ca3ea
Isaiah Harris <harrisfambam@gmail.com>
2019-01-19 16:42:58 -1000

    set up route

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const projects = require('./projects');
 const auth = require('./auth');
+const technicians = require('./technicians');
 
-router.use('/projects', projects);
 router.use('/', auth);
+router.use('/projects', projects);
+router.use('/technicians', technicians);
 module.exports = router;
